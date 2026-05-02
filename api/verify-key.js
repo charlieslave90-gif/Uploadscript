9da9cca97e1d4f0dce7b96da16850d4f1576458b
Kiel <charlieslave90@gmail.com>
2026-05-02 22:57:47 +0800

    Update verify-key.js

--- api/verify-key.js
+++ api/verify-key.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         "Free_XJQPWKDMVRTYNAZBLC",
         "Free_MHATQZPLXCVBNERKJD",
         "Free_UYTRQPLKJHGFDSAZXC",
-        "Free_NVBXQWERTYPLMKJHGF",
+        "FREE_1829c98f814c4",
         "Free_ZXCVBNMASDFGHJKLQP",
         "Free_QWPLMNZXCVAKJHGFDT"
     ];
